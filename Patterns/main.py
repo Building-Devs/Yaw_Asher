c59167b4863da3676f9d66044bfb5778eec0f08d
4k0s4h <101297641+4k0s4h@users.noreply.github.com>
2022-07-19 16:46:10 +0000

    Corrected typos in comments.

--- Patterns/main.py
+++ Patterns/main.py
@@ -3,19 +3,22 @@ Asher,
      I am not very experienced with functions so you will not see any.
     As a result I have to repeat a lot of code.
 """
-# This take input from the user within the range 1 to 5.
+# This takes input from the user within the range 1 to 5.
 num = int(input("Input any number from 1 to 5: "))
-# This starts the condition for the defined range.
 
+# This starts the conditions for the defined range.
 if num < 6: # This block will be executed only if the input falls within the specified range.
+
     if num == 1: # This is for when the input is 1.
         # Asher, I will need your help with this pattern.
         print()
+
     elif num == 2: # This is for when the input is 2.
         for row in range(5):
             for col in range(row + 1):
                 print('*', end=' ')
             print()
+
     elif num == 3: # This is for when the input is 3.
         for r in range(5):
             for c in range(9):
@@ -24,9 +27,11 @@ if num < 6: # This block will be executed only if the input falls within the spe
                 else:
                     print(' ', end=' ')
             print()
+
     elif num == 4: # This is for when the input is 4.
         # Asher, I will also need your help here.
         print()
+
     elif num == 5: # This is for when the input is 5.
         for r in range(num):
             for c in range(9):
@@ -38,5 +43,6 @@ if num < 6: # This block will be executed only if the input falls within the spe
                     print(' ', end=' ')
             print()
 
-else: # This message appears when the input is out of range.
+# This message appears when the input is out of range.
+else:
     print("This input is out of range.")
