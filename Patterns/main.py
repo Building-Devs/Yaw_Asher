b94d8bd031f2ceee053eba896dc264f68acb25aa
4k0s4h <101297641+4k0s4h@users.noreply.github.com>
2022-07-19 15:40:49 +0000

    Initial commit

---
+++ Patterns/main.py
@@ -0,0 +1,45 @@
+
+num = 5 #int(input("Input any number from 1 to 5: "))
+"""
+for i in range(num):
+    for j in range(i+1):
+        print('*', end=' ')
+    print()
+"""
+"""
+if num < 6:
+    for r in range(num-1):
+        for c in range(r, num):
+            print(' ', end=' ')
+        for c in range(r+1):
+            print('*', end=' ')
+        for c in range(r):
+            print('*', end=' ')
+
+        #  for c in range(r):
+       #     print('*', end=' ')
+
+        print()
+    for r in range(num):
+        for c in range(r+1):
+            print(' ', end=' ')
+        for c in range(r, num):
+            print('*', end=' ')
+        for c in range(r, num-1):
+            print('*', end=' ')
+        print()
+
+else:
+    print("The input is out of range.")
+"""
+
+
+for r in range(num):
+    for c in range(9):
+        if (r + c == 4) or (c - r == 4):
+            print('*', end=' ')
+        elif (c == 4):
+            print('1', end=' ')
+        else:
+            print(' ', end=' ')
+    print()
